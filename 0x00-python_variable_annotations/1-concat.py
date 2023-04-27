6d430e40e28d79e6f89ab150b484160a675e3d96
Stephen Obialor <stevobialor@gmail.com>
2023-04-27 14:28:52 +0000

    return concatenation of two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+contains the concat method
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+    returns concatenation of the two strings
+    '''
+    return (str1 + str2)
