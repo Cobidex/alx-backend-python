51ac8c301ee54ac71b179182740a636390458af3
Stephen Obialor <stevobialor@gmail.com>
2023-04-27 14:34:51 +0000

    return floor of a float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Takes a float n as argument and returns the floor of the float.
+"""
+import math
+
+def floor(n: float) -> int:
+    """
+    Takes a float n as argument and returns the floor of the float.
+
+    Args:
+        n (float): The float argument.
+
+    Returns:
+        int: The floor of n as an integer.
+    """
+    return math.floor(n)
