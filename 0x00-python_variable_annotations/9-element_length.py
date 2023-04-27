049da0af1b39bece5579355e5af7dc6f109b0819
Stephen Obialor <stevobialor@gmail.com>
2023-04-27 17:50:30 +0000

    return list of integer tuples

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+contains the element_length function
+'''
+from typing import List, Tuple, Iterable, Sequence
+
+
+def element_length(lst: List[int]) -> List[Tuple[Sequence, int]]:
+    '''
+    returns a list of integer tupples
+    '''
+    return [(i, len(i)) for i in lst]
