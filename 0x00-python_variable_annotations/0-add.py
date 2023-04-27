a287f266cdf5e592e55559df3f04c7b21128c927
Stephen Obialor <stevobialor@gmail.com>
2023-04-27 14:23:27 +0000

    return sum of two floats

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 '''
-contains the add function
+Add two float arguments and return their sum as a float.
 '''
 
 def add(a: float, b: float) -> float:
     '''
-    returns sum of two floats
+    Add two float arguments and return their sum as a float.
+
+    Args:
+        a (float): The first float argument.
+        b (float): The second float argument.
+
+    Returns:
+        float: The sum of a and b.
     '''
     return a + b
