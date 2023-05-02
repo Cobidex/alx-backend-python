9727ce5c7177843672d45ce9d6e03ad348acb1f0
Stephen Obialor <stevobialor@gmail.com>
2023-05-02 14:42:04 +0000

    returns a task object

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -3,12 +3,10 @@
 contains task_wait_random
 '''
 import asyncio
-from typing import TypeVar
 wait_random = __import__('0-basic_async_syntax').wait_random
-T = TypeVar('T')
 
 
-def task_wait_random(max_delay: int) -> T:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     '''
     task_wait_random - regular function
     args:
