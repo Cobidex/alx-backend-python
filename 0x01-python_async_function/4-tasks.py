82a9ba03031c681d02f715743741e2c982fea438
Stephen Obialor <stevobialor@gmail.com>
2023-05-02 20:55:48 +0100

    done

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-
+'''
+contains the task_wait_n
+'''
 import asyncio
 import random
 from typing import List
@@ -9,6 +11,9 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    '''
+    returns a list of delats
+    '''
     delays = []
     for i in range(n):
         task = task_wait_random(max_delay)
