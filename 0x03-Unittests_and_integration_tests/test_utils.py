239b2b0c0b1c19ef1517e6f90058f190586598e2
Stephen Obialor <stevobialor@gmail.com>
2023-05-20 10:34:23 +0000

    make script importable

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -12,14 +12,10 @@ class TestAccessNestedMap(unittest.TestCase):
     class containing test method for the utils.access_nested_map method
     '''
     @parameterized.expand([
-        ({'a': 1}, ('a',), 1),
-        ({'a': {'b': 2}}, ('a',), {'b': 2}),
-        ({'a': {'b': 2}}, ('a', 'b',), 2)
+        ({'a': 1}, ['a'], 1),
+        ({'a': {'b': 2}}, ['a'], {'b': 2}),
+        ({'a': {'b': 2}}, ['a', 'b'], 2)
         ])
     def test_access_nested_map(self, collection, keys, value):
         '''tests the access_nested_map method'''
         self.assertEqual(access_nested_map(collection, keys), value)
-
-
-if __name__ == '__main__':
-    unittest.run()
