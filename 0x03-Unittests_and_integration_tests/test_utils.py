acf9e85c6e32ce839d97736ab272cde66516ef18
Stephen Obialor <stevobialor@gmail.com>
2023-05-20 11:19:06 +0000

    check if key error is raised

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -13,14 +13,24 @@ class TestAccessNestedMap(unittest.TestCase):
     class containing test method for the utils.access_nested_map method
     '''
     @parameterized.expand([
-        ({'a': 1}, ('a',), 1),
-        ({'a': {'b': 2}}, ('a',), {'b': 2}),
-        ({'a': {'b': 2}}, ('a', 'b',), 2)
+        ({'a': 1}, ['a'], 1),
+        ({'a': {'b': 2}}, ['a'], {'b': 2}),
+        ({'a': {'b': 2}}, ['a', 'b'], 2)
         ])
     def test_access_nested_map(self, collection: dict,
                                keys: list, value: Any) -> None:
         '''tests the access_nested_map method'''
         self.assertEqual(access_nested_map(collection, keys), value)
+
+    @parameterized.expand([
+        ({}, ['a']),
+        ({'a': 1}, ['a', 'b'])
+        ])
+    def test_access_nested_map_exception(self, collection: dict,
+                                         keys: list) -> None:
+        '''checks if the KeyError exception is raised'''
+        with self.assertRaises(KeyError):
+            access_nested_map(collection, keys)
 
 
 if __name__ == "__main__":
