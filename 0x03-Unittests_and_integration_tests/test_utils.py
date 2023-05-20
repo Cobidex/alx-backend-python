b9240bfa795e158785621dae5981302e23a2f328
Stephen Obialor <stevobialor@gmail.com>
2023-05-20 12:17:50 +0000

    test the get_json method of utils module

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,7 +5,8 @@ contains the test class TestAccessNestedMap
 import unittest
 from typing import Any
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
+from unittest.mock import patch, MagicMock
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -30,6 +31,25 @@ class TestAccessNestedMap(unittest.TestCase):
                                          keys: list) -> None:
         '''checks if the KeyError exception is raised'''
         self.assertRaises(KeyError, access_nested_map, collection, keys)
+
+
+class TestGetJson(unittest.TestCase):
+    '''
+    contains methods to test the get_json methods of utils module
+    '''
+    @parameterized.expand([
+        ("http://example.com", {'payload': True}),
+        ("http://holberton.io", {"payload": False})
+        ])
+    @patch('utils.requests')
+    def test_get_json(self, url: str, payload: dict, mock_requests,) -> dict:
+        '''checks if expected json is retrieved'''
+        mock_response = MagicMock()
+        mock_response.status_code = 200
+        mock_response.json.return_value = payload
+        mock_requests.get.return_value = mock_response
+
+        self.assertEqual(get_json(url), payload)
 
 
 if __name__ == "__main__":
