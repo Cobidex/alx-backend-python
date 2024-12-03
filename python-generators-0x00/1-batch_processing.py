b4e9a7012672f292574a20fe2c2ac5829ce82909
Stephen <stevobialor@gmail.com~>
2024-12-03 22:13:50 +0100

    add: batch retriever and processor

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,21 @@
+db_connection = __import__('seed.py').connect_to_prodev()
+
+def stream_users_in_batches(batch_size):
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT COUNT(*) FROM user_data")
+    num_users = cursor.fetchone()[0]
+    num_batches = num_users // batch_size
+    for i in range(num_batches):
+        cursor.execute(f"SELECT * FROM user_data LIMIT {i * batch_size}, {batch_size}")
+        users = cursor.fetchall()
+        yield users
+
+    cursor.execute(f"SELECT * FROM user_data LIMIT {num_batches * batch_size}, {num_users % batch_size}")
+    users = cursor.fetchall()
+    yield users
+
+def batch_processing():
+    for batch in stream_users_in_batches(10):
+        for user in batch:
+            if user[3] > 25:
+                print(user)
