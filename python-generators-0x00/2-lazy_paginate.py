016ea68763216576a7f65bac9f550e1ac5c175d1
Stephen <stevobialor@gmail.com~>
2024-12-04 20:09:15 +0100

    add: the paginate_users to the function

--- python-generators-0x00/2-lazy_paginate.py
+++ python-generators-0x00/2-lazy_paginate.py
@@ -13,3 +13,11 @@ def lazy_paginate(page_size):
         yield rows
     cursor.close()
     connection.close()
+
+def paginate_users(page_size, offset):
+    connection = seed.connect_to_prodev()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(f"SELECT * FROM user_data LIMIT {page_size} OFFSET {offset}")
+    rows = cursor.fetchall()
+    connection.close()
+    return rows
