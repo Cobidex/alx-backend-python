3d9065cae1acf60f7044f9500a07d659a05d09b2
Stephen <stevobialor@gmail.com~>
2024-12-04 20:07:10 +0100

    add: lazy loading with yield

---
+++ python-generators-0x00/2-lazy_paginate.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+seed = __import__('seed')
+
+def lazy_paginate(page_size):
+    connection = seed.connect_to_prodev()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT COUNT(*) FROM user_data")
+    total_rows = cursor.fetchone()[0]
+    total_pages = total_rows // page_size
+    for page in range(total_pages + 1):
+        cursor.execute(f"SELECT * FROM user_data LIMIT {page_size} OFFSET {page_size * page}")
+        rows = cursor.fetchall()
+        yield rows
+    cursor.close()
+    connection.close()
