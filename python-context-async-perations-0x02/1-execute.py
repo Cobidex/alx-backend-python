467e751cc9853df48d9129afa06ca239b15d2fe9
Stephen <stevobialor@gmail.com~>
2024-12-11 10:57:48 +0100

    implement class based custom context manager

---
+++ python-context-async-perations-0x02/1-execute.py
@@ -0,0 +1,18 @@
+class ExecuteQuery:
+    def __init__(self, db_name, query, params=None):
+        self.db_name = db_name
+        self.query = query
+        self.params = params if params else []
+        self.connection = None
+        self.result = None
+
+    def __enter__(self):
+        self.connection = sqlite3.connect(self.db_name)
+        cursor = self.connection.cursor()
+        cursor.execute(self.query, self.params)
+        self.result = cursor.fetchall()
+        return self.result
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.connection:
+            self.connection.close()
