d5d91965d091e92b3ceb09c1c58535c837e85303
Stephen <stevobialor@gmail.com~>
2024-12-18 20:40:58 +0100

    add: default authentication classes

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -55,9 +55,11 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
 }
 
 ROOT_URLCONF = 'messaging_app.urls'
