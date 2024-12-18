4ee9074a8e6f39a72cbb2260a2f3f46cee67ccb8
Stephen <stevobialor@gmail.com~>
2024-12-18 23:50:21 +0100

    done with models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -7,6 +7,7 @@ class User(AbstractUser):
     first_name = models.CharField(max_length=150, null=False)
     last_name = models.CharField(max_length=150, null=False)
     email = models.EmailField(unique=True, null=False)
+    password = models.CharField(max_length=128, null=False)
     phone_number = models.CharField(max_length=15, null=True, blank=True)
     role = models.CharField(
         max_length=10,
