add7b956f0a98b496144faec45afbdf6507e7069
Stephen <stevobialor@gmail.com~>
2025-01-11 10:28:31 +0100

    endpoint to retrieve unread chats

--- Django-signals_orm-0x04/messaging/managers.py
+++ Django-signals_orm-0x04/messaging/managers.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class UnreadMessagesManager(models.Manager):
-    def get_queryset(self):
+    def unread_for_user(self):
         return super().get_queryset().filter(read=False).only('message_id', 'sender', 'receiver', 'content', 'timestamp', 'parent_message')
